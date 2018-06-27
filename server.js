78dce67d47b78110934fe5538f5e3efecb8f4bf0
AdrianLayton <AdrianLayton@gmail.com>
2018-06-27 12:58:57 -0400

    Added package.json and added bug testing for app.listen

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.get('/', function(req, res){
    res.render("Hello World!");
 });
-const privateIP = 172.31.25.246
-app.listen(3000, 'privateIP', () = > {
+const privateIP = '172.31.25.246'
+app.listen(3000, privateIP, () = > {
 	console.log(`Listening on ${privateIP}:3000`)
 });
